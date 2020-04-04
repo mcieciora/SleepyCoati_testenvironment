70782539d5ea5d2dbe95b631ce55eb5d0bda695d
mcieciora <michalcieciora96@gmail.com>
2020-04-04 10:53:15 +0200

    04042020-1053

--- executor.py
+++ executor.py
@@ -1,13 +1,16 @@
-import os
+from os import getcwd, getenv
+from os.path import join
+from sys import path
 import unittest
 import xmlrunner
 
 # Jenkins variables
-scope = os.getenv('scope')
+scope = getenv('scope')
 
 # Project paths
-base_path = os.getcwd()
+base_path = getcwd()
 
+path.insert(0, join(base_path, 'app'))
 
 patterns_list = ['smoke_*.py', 'test_*.py']
 if scope == 'smoke':
@@ -20,7 +23,7 @@ def discover_test_cases():
     test_loader = unittest.TestLoader()
     test_suite = unittest.TestSuite()
     for pattern in patterns_list:
-        test_suite.addTest(test_loader.discover(os.path.join(base_path, 'TestCases'), pattern))
+        test_suite.addTest(test_loader.discover(join(base_path, 'TestCases'), pattern))
     return test_suite
 
 
