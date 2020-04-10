5cfe45f6e95503dbe4c857d704ac6e0d01539456
mcieciora <michalcieciora96@gmail.com>
2020-04-10 12:22:23 +0200

    10042020-1222

--- TestCases/get_list_elements_type/test_get_list_elements_type.py
+++ TestCases/get_list_elements_type/test_get_list_elements_type.py
@@ -6,23 +6,23 @@ from os import getenv
 
 class Test(unittest.TestCase):
     def setUp(self) -> None:
+        self.parser = Parser(None, None)
         self.expression_list_str = ['variable_name']
         self.expression_list_int = ['variable_name', 5]
         self.expression_list_bool = ['variable_name', True]
 
     def test_get_list_elements_type_empty_list(self):
-        self.assertEqual(Parser.get_list_elements_type([]), []), \
+        self.assertEqual(self.parser.get_list_elements_type([]), []), \
             'test_get_list_elements_type_empty_list'
 
     def test_get_list_elements_type_str_value(self):
-        self.assertEqual(Parser(self.expression_list_str, join(getenv('WORKSPACE'), 'TestCases',
-                                                               'get_list_elements_type', 'command.xml')), [str]), \
+        self.assertEqual(self.parser.get_list_elements_type(self.expression_list_str), [str]), \
             'test_get_list_elements_type_str_value'
 
     def test_get_list_elements_type_int_value(self):
-        self.assertEqual(Parser(self.expression_list_int, 'command.xml'), [int]), \
+        self.assertEqual(self.parser.get_list_elements_type(self.expression_list_int), [str, int]), \
             'test_get_list_elements_type_int_value'
 
     def test_get_list_elements_type_bool_value(self):
-        self.assertEqual(Parser(self.expression_list_bool, 'command.xml'), [bool]), \
+        self.assertEqual(self.parser.get_list_elements_type(self.expression_list_bool), [str, bool]), \
             'test_get_list_elements_type_bool_value'
