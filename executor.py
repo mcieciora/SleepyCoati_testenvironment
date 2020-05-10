3009ca85bb223ff7570a2eafc7df6631cac67784
Michał Cieciora <michalcieciora96@gmail.com>
2020-05-10 20:25:04 +0200

    10052020-2025

--- executor.py
+++ executor.py
@@ -7,21 +7,24 @@ import xmlrunner
 # Project paths
 base_path = getcwd()
 components = environ['components']
-print(components)
 path.insert(0, join(base_path, 'app'))
 
 
-def discover_test_cases():
-    test_loader = unittest.TestLoader()
-    test_suite = unittest.TestSuite()
-    for component in get_components():
-        test_suite.addTest(test_loader.discover(join(base_path, 'TestCases', component), 'test_*.py'))
-    return test_suite
+def discover_test_cases(sub_test_case):
+    try:
+        test_loader = unittest.TestLoader()
+        test_suite = unittest.TestSuite()
+        if sub_test_case != '':
+            test_suite.addTest(test_loader.discover(join(base_path, 'TestCases', sub_test_case), 'test_*.py'))
+        return test_suite
+    except ImportError:
+        return unittest.TestSuite()
 
 
 def get_components():
     return components.split(',')
 
 
-runner = xmlrunner.XMLTestRunner(output='TestResults')
-result = runner.run(discover_test_cases())
+for component in get_components():
+    runner = xmlrunner.XMLTestRunner(output='TestResults')
+    result = runner.run(discover_test_cases(component))
