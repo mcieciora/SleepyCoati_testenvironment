9d7a1bb19729b983d498ad7b8ec7480515079a6e
mcieciora <michalcieciora96@gmail.com>
2020-06-07 18:03:41 +0200

    07062020-1803

--- TestCases/TestCase_Unittest/evaluate_body_values/test_evaluate_body_values.py
+++ TestCases/TestCase_Unittest/evaluate_body_values/test_evaluate_body_values.py
@@ -11,25 +11,25 @@ class Test(unittest.TestCase):
         self.expression_list_bool = ['True', 'False']
         self.expression_list_mix = ['string', 'True', '5']
 
-    def test_value_evaluation_basic_value(self):
+    def test_evaluate_body_values_basic_value(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_basic), ['variable_name', 3]), \
             'test_value_evaluation_basic_value'
 
-    def test_value_evaluation_empty_strings(self):
+    def test_evaluate_body_values_empty_strings(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_empty_strings), []), \
             'test_value_evaluation_empty_strings'
 
-    def test_value_evaluation_empty_list(self):
+    def test_evaluate_body_values_empty_list(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_empty_list), []), \
             'test_value_evaluation_empty_list'
 
-    def test_value_evaluation_ints(self):
+    def test_evaluate_body_values_ints(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_ints), [3, 4]), 'test_value_evaluation_ints'
 
-    def test_value_evaluation_bools(self):
+    def test_evaluate_body_values_bools(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_bool), [True, False]), \
             'test_value_evaluation_bools'
 
-    def test_value_evaluation_mix(self):
+    def test_evaluate_body_values_mix(self):
         self.assertEqual(Parser.evaluate_body_values(self.expression_list_mix), ['string', True, 5]), \
             'test_value_evaluation_mix'
--- TestCases/TestCase_Unittest/xml_file_path/test_xml_file_path.py
+++ TestCases/TestCase_Unittest/xml_file_path/test_xml_file_path.py
@@ -5,6 +5,7 @@ from app.Parser import Parser
 
 class Test(unittest.TestCase):
     def setUp(self) -> None:
+        print('getenv: ', getenv('WORKSPACE'))
         self.xml_file_path_directory = path.join(getenv('WORKSPACE'), 'TestCases', 'TestCase_Unittest', 'xml_file_path')
 
     def test_xml_file_path_relative_path(self):
@@ -13,6 +14,7 @@ class Test(unittest.TestCase):
             'test_xml_file_path_relative_path'
 
     def test_xml_file_path_absolute_path(self):
+        abc = path.join(self.xml_file_path_directory, 'commands.xml')
         parser = Parser('empty.co', path.join(self.xml_file_path_directory, 'commands.xml'))
         self.assertEqual(parser.xml_file_path, path.join(self.xml_file_path_directory, 'commands.xml')), \
             'test_xml_file_path_absolute_path'
@@ -34,6 +36,6 @@ class Test(unittest.TestCase):
 
     def test_xml_file_path_double_dot(self):
         chdir('directory')
-        parser = Parser('empty.co', '../empty.xml')
+        parser = Parser('empty.co', '../commands.xml')
         self.assertEqual(parser.xml_file_path, path.join(self.xml_file_path_directory, 'commands.xml')), \
             'test_xml_file_path_double_dot'
